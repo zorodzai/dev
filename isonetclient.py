daeda669bb7f6d20dfd641f16e9ed8fee137d32f
zorodzai <zorodzai@gmail.com>
2018-02-13 13:49:18 +0200

    updated isoclient

--- isonetclient.py
+++ isonetclient.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python
 import socket
+import sys
+import time
+from ISO8583.ISO8583 import ISO8583
+from ISO8583.ISOErrors import *
 
-s = socket.socket()
-host = socket.gethostname()
-port = 26000
+serverIP = socket.gethostname()
+serverPort = 26000
+numberEcho = 10
+timeBetweenEcho = 5
 
-s.connect((host, port))
-print("connected")
-print(s.recv(1024))
-s.close
+bigEndian = True
+s = None
+
+for res in socket.getaddrinfo(serverIP,serverPort,socket.AF_UNSPEC,socket.SOCK_STREAM):
+    af, socktype, proto, canonname, sa = res
+    try:
+        s = socket.socket(af,socktype,proto)
+    except socket.error, msg:
+        s=None
+        continue
+    try:
+        s.connect(sa)
+    except socket.error, msg:
+        s.close()
+        s=None
+        continue
+    break
+if s is None:
+    print("Could not connect")
+    sys.exit(1)
