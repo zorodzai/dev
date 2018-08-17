7a6ac08c6fbf4aa14851d9c959019d17a60bd126
Zorodzai Mhlanga <zorodzai@gmail.com>
2018-08-17 19:29:18 +0200

    Cash count added

--- card_extract/card_extract.py
+++ card_extract/card_extract.py
@@ -1,5 +1,4 @@
 #Utility to parse Postcard card-accounts extract files to create output file of active cards
-
 import pandas as pd
 cardspath = r'C:\Zoro\dev\python\card_extract\uob\cards.txt'
 cardaccountspath = r'C:\Zoro\dev\python\card_extract\uob\cardaccounts.txt'
@@ -7,8 +6,10 @@ output=r'C:\Zoro\dev\python\card_extract\uob\output.txt'
 
 #only cards with expiry date >= 'period' to be returned
 period = int(input("Enter date in YYMM of last expiry: "))
-
+print(" ")
 #clean extract files of apostrophes around data (Standard 3 format)
+
+
 def cardcleaner():
 	try:
 		with open(cardspath,'r') as file:
@@ -19,7 +20,8 @@ def cardcleaner():
 		print("cleaning card data complete")
 	except:
 		print("error occured in cleaning cards file")
-		
+
+
 def cardaccountscleaner():
 	try:
 		with open(cardaccountspath,'r') as file:
@@ -37,11 +39,12 @@ print("\nextracting\n")
 
 #use pandas to filter data and merge into one file
 df = pd.read_csv(cardspath,usecols=[0,4,6,7],names=['a','b','c','d'])
-hold=['41.0','43.0','1.0']
+hold=['41.0','42.0','43.0','1.0','45.0','5.0']
 df0=df[df.b >= 1]
 df1 = df0[df0.c >= period]
 dfcards = df1[~df1.d.isin(hold)]
 dfcardaccounts = pd.read_csv(cardaccountspath,usecols=[0,2,3],names=['a','b','c'])
 merged=pd.merge(dfcards,dfcardaccounts,on='a',how='inner')
 merged.to_csv(output,header=False,index=False)
+
 print("extraction complete")
---
+++ scratchpad.py
@@ -0,0 +1,17 @@
+'''def BMI(kg,m):
+	BMI = int(kg)/(float(m)**2)
+	print(BMI)
+kg = input("Enter your weight: ")
+m = input ("Enter your height: ")
+BMI(kg,m)'''
+
+x = 1 / 2 + 3 // 3 + 4 ** 2
+print(x)
+
+
+x=int(11)
+y=int(4)
+x = x % y
+x= x % y
+y = y % x
+print(y)
